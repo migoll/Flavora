52fe4a9ff2cccad6c2900056a9ce440a4999cdd4
Gabbana <chris.lund02@gmail.com>
2024-04-26 11:06:11 +0200

    added all js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 const baseUrl = "https://api.chrlund.com/wp-json/wp/v2/";
-const recipeCategoryId = 1;
+const recipeCategoryId = 3;
 
 const containerEl = document.querySelector(".container");
 
@@ -37,7 +37,38 @@ function getPrivateRecipes(){
     }
   })
   .then(res => res.json())
-  .then(recipes => {/* noget her */ 
+  .then(recipes => {
+    recipes.forEach(recipe => {
+      //create div, h2 for each recipe
+      const recipeEl = document.createElement("div");
+      const titleEl = document.createElement("h2");
+      const contentEl = document.createElement("div");
+      const aboutEl = document.createElement("p");
+      const ingredientEl = document.createElement("ul");
+      const preperationEl = document.createElement("ol");
+      const preperationLiEl = document.createElement("li")
+      const ingredientLiEl = document.createElement("li")
+
+      // add content to elements
+      titleEl.textContent = recipe.acf.title
+      aboutEl.textContent = recipe.acf.about
+      ingredientLiEl.textContent = recipe.acf.ingredients
+      preperationLiEl.textContent = recipe.acf.preperation
+      
+      recipeEl.classList.add(".recipeStyling")
+
+      //append elements into containers
+      recipeEl.appendChild(titleEl);
+      recipeEl.appendChild(contentEl);
+      contentEl.appendChild(aboutEl);
+      contentEl.appendChild(ingredientEl);
+      contentEl.appendChild(preperationEl);
+      ingredientEl.appendChild(preperationLiEl);
+      preperationEl.appendChild(ingredientLiEl);
+
+
+      containerEl.appendChild(recipeEl)
+    })
     console.log(recipes);
   })
   .catch(err => console.log("fejl!:", err))
